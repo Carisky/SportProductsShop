797d55f783e9925cb294027086053a61ea6d3864
Yehor <egorkorsun7@gmail.com>
2024-06-01 19:40:46 +0200

    Добавьте файлы проекта.

---
+++ SportProductsShop/wwwroot/js/carousel.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const prevButton = document.getElementById('prevButton');
+    const nextButton = document.getElementById('nextButton');
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    let currentIndex = 0;
+
+    carouselItems[currentIndex].classList.add('active');
+
+    function updateActiveItem() {
+        carouselItems.forEach(item => item.classList.remove('active'));
+        carouselItems[currentIndex].classList.add('active');
+    }
+
+    prevButton.addEventListener('click', function () {
+        currentIndex = (currentIndex === 0) ? carouselItems.length - 1 : currentIndex - 1;
+        updateActiveItem();
+    });
+
+    nextButton.addEventListener('click', function () {
+        currentIndex = (currentIndex === carouselItems.length - 1) ? 0 : currentIndex + 1;
+        updateActiveItem();
+    });
+});
